4e45ae72e239dd8d02b23c8546e46cf44c8e1072
Paul Stilhart <paul.stilhart@gmail.com>
2022-03-10 10:24:16 +0000

    avancée feuille de style

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-//script qui est injecté dans squarespace
+/* 
+https://www.greenisbetter.fr/
+https://drive.google.com/drive/folders/10hEXak3ve2xlOK9dhjzXkpMGE4thnrME
 
+https://cyan-oval-2nyw.squarespace.com/config/pages
+https://bo.zelty.fr/web-widgets
+https://support.zelty.fr/hc/fr/articles/360023772592-Personnalisez-la-commande-en-ligne-%C3%A0-vos-couleurs
+
+https://github.com/Greenisbetter/zelty_CSS
+
+https://cdn.jsdelivr.net/gh/Greenisbetter/zelty_CSS/style.css
+
+https://fonts.google.com/specimen/Bebas+Neue?query=bebas+neue
+regular 400
+https://fonts.google.com/specimen/Arimo#standard-styles
+en medium 500
+*/
+
+
+//script qui est injecté dans squarespace
 <script id="zelty-order-iframe">
 (function(){
 	window._zwg = window._zwg || {}
 	window._zwg.key = '7d158a5fbc135a4'
-    window._zwg.stylesheet = 'https://cdn.jsdelivr.net/gh/Greenisbetter/zelty_CSS/style.css'
+	//lien avec la feuille de style sur jsdeliver
+    window._zwg.stylesheet = 'https://cdn.jsdelivr.net/gh/Greenisbetter/zelty_CSS/style12.css'
 	//window._zwg.theme = 'dark'
 	var z = document.createElement('script')
 	z.type = 'text/javascript'
